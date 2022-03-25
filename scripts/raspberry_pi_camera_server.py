270c7a537a23a6926c540946c8c2e711fd5eaa33
Artur Istvan Karoly <artur.karoly@irob.uni-obuda.hu>
2022-03-25 19:31:56 +0100

    Add documentation

--- scripts/raspberry_pi_camera_server.py
+++ scripts/raspberry_pi_camera_server.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+## @package raspberry_pi_camera
+# The ROS node for controlling the Raspberry PI camera
+#
+# Defines a ROS action server for taking photos with the Raspberry PI camera
+
 import rospy
 import actionlib
 from cv_bridge import CvBridge
@@ -11,27 +16,44 @@ import picamera
 from picamera.array import PiRGBArray
 from fractions import Fraction
 
+## RaspberryPiCameraServer class
+#
+# Defines a ROS action server for taking photos with the Raspberry PI camera
 class RaspberryPiCameraServer:
-  #def __init__(self, width3280, height=2464):
+  '''
+    Class for camera control
+  '''
+
+  ## Constructor of RaspberryPiCameraServer class
+  # @param width int type with of the image (default value is 1920)
+  # @param height int type height of the image (default value is 1080)
   def __init__(self, width=1920, height=1080):
-
+    ## @var width
+    #  Image width
     self.width = width
-    self.height = height
-    # self.camera = picamera.PiCamera()
-    # self.camera.resolution = (width, height)
-    # time.sleep(2)
-    # self.rawCapture = PiRGBArray(self.camera)
 
+    ## @var height
+    #  Image height
+    self.height = height
+
+    ## @var bridge
+    #  CvBridge() object for conversions between numpy arrays and ROS Image message types
     self.bridge = CvBridge()
+
+    ## @var server
+    #  The ROS action server for the RaspberryPiCameraAction action. The name of the action is "raspberry_pi_camera"
     self.server = actionlib.SimpleActionServer('raspberry_pi_camera', RaspberryPiCameraAction, self.execute, False)
     self.server.start()
 
-
-
-  # def __del__(self):
-  #   self.camera.close()
-
+  ## RaspberryPiCameraAction callback
+  # This function gets called whenever the ROS action server receives a goal from a client
+  # @param goal bark_msgs/RaspberryPiCameraGoal type action goal, it is an empty message to signal the need for an image (see action definition for further details)
   def execute(self, goal):
+    '''
+    RaspberryPiCameraAction callback
+
+    goal: bark_msgs/RaspberryPiCameraGoal, action goal, it is an empty message to signal the need for an image (see action definition for further details)
+    '''
     camera = picamera.PiCamera()
     camera.resolution = (self.width, self.height)
     camera.iso = 100
