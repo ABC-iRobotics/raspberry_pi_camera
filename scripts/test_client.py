2694c350ea8dfe7d15db72fc16a896e47bf0791e
Sebestyen <tirczkas@gmail.com>
2022-09-23 11:06:16 +0200

    Add .gitignore file and impruve test client

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -3,6 +3,8 @@
 import rospy
 import actionlib
 from cv_bridge import CvBridge
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from bark_msgs.msg import RaspberryPiCameraAction, RaspberryPiCameraGoal
 
@@ -16,5 +18,9 @@ if __name__ == '__main__':
     goal = RaspberryPiCameraGoal()
     # Fill in the goal here
     client.send_goal(goal)
-    client.wait_for_result(rospy.Duration.from_sec(5.0))
-    print(client.get_result())
+    client.wait_for_result(rospy.Duration.from_sec(30.0))
+    # print(client.get_result())
+    
+    img = bridge.imgmsg_to_cv2(client.get_result().image, "bgr8")
+    plt.imshow(img)
+    plt.show()
