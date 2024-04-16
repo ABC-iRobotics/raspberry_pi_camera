640cd25b8af4dc08ebe6cf38b9ed6976db5f6c58
tarsoly <tarsoly@yahoo.com>
2024-04-16 15:26:31 +0200

    camera preview is added

--- scripts/raspberry_pi_camera_server.py
+++ scripts/raspberry_pi_camera_server.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python
 
-## @package raspberry_pi_camera
-# The ROS node for controlling the Raspberry PI camera
-#
-# Defines a ROS action server for taking photos with the Raspberry PI camera
-
+from pickletools import uint8
+from unittest import skip
 import rospy
 import actionlib
 from cv_bridge import CvBridge
@@ -17,52 +14,43 @@ from picamera.array import PiRGBArray
 from fractions import Fraction
 import cv2
 
-## RaspberryPiCameraServer class
-#
-# Defines a ROS action server for taking photos with the Raspberry PI camera
 class RaspberryPiCameraServer:
-  '''
-    Class for camera control
-  '''
-
-  ## Constructor of RaspberryPiCameraServer class
-  # @param width int type with of the image (default value is 1920)
-  # @param height int type height of the image (default value is 1080)
   def __init__(self, width=1920, height=1080):
-    ## @var width
-    #  Image width
     self.width = width
-
-    ## @var height
-    #  Image height
     self.height = height
 
-    ## @var bridge
-    #  CvBridge() object for conversions between numpy arrays and ROS Image message types
     self.bridge = CvBridge()
-
-    ## @var server
-    #  The ROS action server for the RaspberryPiCameraAction action. The name of the action is "raspberry_pi_camera"
     self.server = actionlib.SimpleActionServer('raspberry_pi_camera', RaspberryPiCameraAction, self.execute, False)
     self.server.start()
 
-  ## RaspberryPiCameraAction callback
-  # This function gets called whenever the ROS action server receives a goal from a client
-  # @param goal bark_msgs/RaspberryPiCameraGoal type action goal, it is an empty message to signal the need for an image (see action definition for further details)
-  def execute(self, goal):
-    '''
-    RaspberryPiCameraAction callback
 
-    goal: bark_msgs/RaspberryPiCameraGoal, action goal, it is an empty message to signal the need for an image (see action definition for further details)
-    '''
+  def execute(self, goal):
     camera = picamera.PiCamera()
-    time.sleep(0.1) # camera warm up
-    
+    camera.start_preview()
+    time.sleep(2) # camera warm up
+
     camera.resolution = (1920, 1088)  # it is necessary to give 1088 height to a right image
+    # camera.iso = 100
+    # time.sleep(2) # important sleep time
+
+    # camera.exposure_mode = 'off'
+    camera.awb_mode = 'auto'
+    camera.awb_gains = (1.5, 3)
+    # camera.shutter_speed = 20000
+    # camera.iso = 200
+
+    rospy.sleep(2)
+
+    print(float(camera.awb_gains[0]), float(camera.awb_gains[1]))
+    print(camera.exposure_speed)
+    print(camera.analog_gain)
+    print(camera.digital_gain)
+    print(camera.exposure_mode)
+
     rawCapture = PiRGBArray(camera, size=(1920,1088))
     camera.capture(rawCapture, 'rgb')
     image = rawCapture.array
-       
+
     image = np.reshape(image, (1088, 1920, 3))
 
     image = cv2.resize(image, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
